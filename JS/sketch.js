f678053cafc0d132b8a5689505470e107f4c8880
tonyfverdu <tony_verdu@hotmail.com>
2022-05-21 12:07:05 +0200

    andere commit

--- JS/sketch.js
+++ JS/sketch.js
@@ -5,46 +5,49 @@ let particle;
 let xoff = 0;
 let yoff = 10000;
 
-// Class "Limite" simple (2 vectores)
+let randomBackgroundColor = Math.floor(Math.random() * 30);
+
+// Class "Limite - Boundary" simple (2 vectores)
 class Boundary {
 	constructor (x1, x2, y1, y2) {
-		this.a = createVector(x1, y1);
-		this.b = createVector(x2, y2);
+		this.a = createVector(x1, y1);  //  Vector a
+		this.b = createVector(x2, y2);  //  Vector b
 	}
 
-	show() {
+	show() {							//  print 2 Vector
 		stroke(255);
 		line(this.a.x, this.a.y, this.b.x, this.b.y);
 	}
 }
 
+//  Class "Ray" (rayos) desde 
 class Ray {
-	constructor (pos, angle) {
+	constructor (pos, angle) {   //  A ray ist a Vector, wit origin: pos(x, y) y a angle (0deg, 360deg)
 		this.pos   = pos;
 		this.angle = angle;
 
 		this.dir = p5.Vector.fromAngle(this.angle);  // or angle too.
 	}
 
-	show() {
+	show() {					//  method print ray, ein line of vector ray
 		stroke(255);
 		push();
 		translate(this.pos.x, this.pos.y);
-		line(0, 0, this.dir.x * 10, this.dir.y * 10);
+		line(0, 0, this.dir.x, this.dir.y);
 		pop();
 	}
 
-	cast (wall) {
-		const x1 = wall.a.x;
+	cast (wall) {				//  method "cast": intersection de ray with wall.
+		const x1 = wall.a.x;	//  (x1, y1, x2, y2) coordenadas of the wall
 		const y1 = wall.a.y;
 		const x2 = wall.b.x;
 		const y2 = wall.b.y;
 
-		const x3 = this.pos.x;
+		const x3 = this.pos.x;			// (x3, y3, x4, y4) coordenadas of ray-line
 		const y3 = this.pos.y;
 		const x4 = this.pos.x + this.dir.x;
 		const y4 = this.pos.y + this.dir.y;
-
+										//  Algoritmo of intersection 2 lines
 		const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
 		if (den === 0) {
 			return;
@@ -53,7 +56,7 @@ class Ray {
 		const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
 		const u = ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
 
-		if (t > 0 && t < 1 && u > 0) {
+		if (t > 0 && t < 1 && u > 0) {		// point of the intersection.
 			 const pt = createVector();
 			 pt.x = x1 + t * (x2 - x1);
 			 pt.y = y1 + t * (y2 - y1)
@@ -64,7 +67,7 @@ class Ray {
 	}
 }
 
-class Particle {
+class Particle {			// some rays, 360deg, array of objekts rays origen pos (position)
 	constructor () {
 		this.pos = createVector(width / 2, height / 2);
 		this.rays = [];
@@ -73,15 +76,16 @@ class Particle {
 		}
 	}
 
-	update (x, y) {
+	update (x, y) {			
 		this.pos.set(x, y);
+		// randomBackgroundColor = Math.floor(Math.random() * 50);
 	}
 
-	look(walls) {
+	look(walls) {			
 		for (let i = 0; i < this.rays.length; i++) {
 			const ray = this.rays[i];
 			let closest = null;
-			let record = Infinity;
+			let record  = Infinity;
 
 			for (let wall of walls) {
 				const pt = ray.cast(wall);
@@ -95,7 +99,7 @@ class Particle {
 			}
 			if (closest) {
 				colorMode (HSB);
-				stroke((i + frameCount * 3) % 360, 255, 255, (i + frameCount * 3) % 360)
+				stroke((i + frameCount * 3) % 360, 255, 255, (i + frameCount * 3) % 360);
 				//stroke(255, 90);
 				line(this.pos.x, this.pos.y, closest.x, closest.y);
 			}
@@ -103,43 +107,48 @@ class Particle {
 	}
 
 	show() {
-		fill(255);
-		ellipse(this.pos.x, this.pos.y, 1.5);
+		for (let i = 0; i < this.rays.length; i++) {
+			fill((i + frameCount * 3) % 360, 255, 255, (i + frameCount * 3) % 360);
+			ellipse(this.pos.x, this.pos.y, 25);  //  ellipse of point 
+			
+		}
 		for (let ray of this.rays) {
 			ray.show();
 		}
 	}
 }
 
-function setup() 
+function setup() 					//  function setup of p5.js löibrary, initialization objects
 {
 	createCanvas(1600, 804);
-	for (let i = 0; i < 10; i++) {
+	for (let i = 0; i < 12; i++) {
 		let x1 = random(width);
 		let x2 = random(width);
 		let y1 = random(height);
 		let y2 = random(height);
-		walls[i] = new Boundary(x1, y1, x2, y2);
+		walls[i] = new Boundary(x1, y1, x2, y2);  //  array of 12 walls
 	}
-	walls.push(new Boundary(0, 0, width, 0));
+	walls.push(new Boundary(0, 0, width, 0));			//  definition of limit of walls of canvas
 	walls.push(new Boundary(width, 0, width, height));
 	walls.push(new Boundary(width, height, 0, height));
 	walls.push(new Boundary(0, height, 0, 0));
-	particle = new Particle();
+	particle = new Particle();	// Init particles of rays
 }
 
-function draw() {
-	background(0);
-	stroke(0);
+function draw() {	//  function draw() of library p5.js)
+	randomBackgroundColor = noise(Math.floor(Math.random() * xoff )) * 20;
+	console.log('randomBackgroundColor: ' + randomBackgroundColor); 
+	background(randomBackgroundColor);
+	stroke(255);
 	for (let wall of walls) {
-		wall.show();
+		wall.show();			//  Show of walls
 	}
 
-	//particle.update(mouseX, mouseY);
-	particle.update(noise(xoff) * width, noise(yoff) * height);
+	//particle.update(mouseX, mouseY);  // control with mouse
+	particle.update(noise(xoff) * width, noise(yoff) * height);  //  random with noise
 	particle.show();
 	particle.look(walls);
 
-	xoff += 0.01;
-	yoff += 0.01;
+	xoff += 0.015;
+	yoff += 0.015;
 }
